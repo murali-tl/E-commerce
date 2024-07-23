127c25f8e0c603ff8642811a649903e7e9d6336a
murali-tl <murali@tilicho.in>
2024-07-23 14:48:37 +0530

    error logs in Admin flow

--- src/services/adminServices.js
+++ src/services/adminServices.js
@@ -36,6 +36,7 @@ const getAllOrders = async (page, limit, search) => {
         //const orders = await order.findAll();
     }
     catch (err) {
+        console.error(err);
         return { "error": err };
     }
 }
@@ -59,6 +60,7 @@ const getOrderDetails = async (orderId) => {
         return orderDetails;
     }
     catch (err) {
+        console.error(err);
         return { success: false, "error": err };
     }
 }
@@ -112,7 +114,7 @@ const createProduct = async (data) => {
         return { success: true, status: 200, message: 'Product created', data: { product_id: productAdded?.product_id } }; //should we need to send product_details
     }
     catch (err) {
-        console.log(err);
+        console.error(err);
         return { success: false, message: " Error while creating new prodct", err };
     }
 }
@@ -138,7 +140,7 @@ const removeProduct = async (data) => {
         return { success: false, status: 400, message: 'Product not found', data: {} };
     }
     catch (err) {
-        console.log(err);
+        console.error(err);
         return { "error": err };
     }
 }
