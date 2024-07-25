716bb5d62ef62f5ccacc2159fe5ea6bdd7f360c1
murali-tl <murali@tilicho.in>
2024-07-25 12:45:40 +0530

    Fixed validation issue in cart

--- src/services/validations.js
+++ src/services/validations.js
@@ -94,7 +94,7 @@ function validateCartDetails(product) {
     product_id: Joi.string().uuid().required(),
     size_id: Joi.string().uuid().required(),
     color_id: Joi.string().uuid().required(),
-    quantity: Joi.number().integer().min(1).required()
+    quantity: Joi.number().integer().min(1)
   });
   return cartDetailSchema.validate(product);
 }
