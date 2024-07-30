03a0cb40e9e252351f9f1a67f8dacd16c10f9419
murali-tl <murali@tilicho.in>
2024-07-30 17:00:08 +0530

    Updated port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const YAML = require("yamljs");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 const cors = require('cors')
 const myRoute = require('./routes/mainRoutes');
